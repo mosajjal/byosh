3ddf550fa55c03d2cc45b7c6b3022e2b4f003bab
Alireza Mosajjal <mosajjal@gmail.com>
2020-04-12 22:46:41 +1200

    remove entrypoint.sh not needed stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# "fix" nginx.conf to include the IP adress
-
-sed -i -e "s/PUB_IP_FROM_ENV/$PUB_IP/g" /etc/nginx/nginx.conf
- 
 pkill -9 nginx
 /usr/sbin/nginx
 # Start the second process
